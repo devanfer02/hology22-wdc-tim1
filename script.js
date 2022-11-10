e67d3e60189b904f66944aeb36e1f25a9fccdcb2
Devan F <116249842+devanfer02@users.noreply.github.com>
2022-11-10 18:20:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const menuClick = document.querySelector("sidebar-menu__item");
 
+function tweetFunction(){
+    let userInput = document.querySelector("#userInput");
+    let tweet = document.querySelector("#tweet");
+
+    tweet.innerHTML = userInput.value;
+}
+
+
 menuClick.addEvent
+
+
